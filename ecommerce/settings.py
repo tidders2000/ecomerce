c3e6d0e97d432a599d8cec105f7b0353c8100fd1
Simon Tidman <simon.tid@btinternet.com>
2019-06-02 15:14:43 +0000

    typpo in allowed host corrected

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'https://tidders-shop-example.herokuapp.com/']
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'https://tidders-shop-example.herokuapp.com']
 
 
 # Application definition
